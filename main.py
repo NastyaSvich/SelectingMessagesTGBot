b58bf3168a4d434203b72f4e94e7c39ba7283df0
ASvich <ASvich@qulix.com>
2024-09-18 00:01:25 +0300

    Добавление функции проверки бота. Актуализация описания

--- main.py
+++ main.py
@@ -12,12 +12,19 @@ SEARCH_WORDS = os.getenv("SEARCH_WORDS")
 
 client = TelegramClient('spainbotsecure', int(API_ID), API_HASH)
 
-
+# Ищет конкретные сообщения в чате и переотправляет их в другой чат.
 @client.on(events.NewMessage(chats=[int(LISTENING_CHANNEL_ID)]))
-async def my_event_handler(event):
+async def search_new_message(event):
     for word in SEARCH_WORDS.split(","):
         if word in event.raw_text:
             await client.send_message(int(SEND_CHANNEL_ID), event.raw_text)
+
+
+# Проверка работоспособности.
+# Ожидает сообщение "/check" в чате указанного для отправки сообщений.
+@client.on(events.NewMessage(chats=[int(SEND_CHANNEL_ID)], pattern='/check'))
+async def check(event):
+    await client.send_message(int(SEND_CHANNEL_ID), 'ACTIVE')
 
 
 client.start()
