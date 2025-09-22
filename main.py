0a80cd86a2a4194f581535e7df5686f46fe8d145
NastyaSvich <56764613+NastyaSvich@users.noreply.github.com>
2025-09-22 12:06:10 +0300

    Исправление чтения каналов с id на наименование

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ load_dotenv()
 
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
-LISTENING_CHANNEL_ID = os.getenv("LISTENING_CHANNEL_ID")
-SEND_CHANNEL_ID = os.getenv("SEND_CHANNEL_ID")
+LISTENING_CHANNEL_NAME = os.getenv("LISTENING_CHANNEL_NAME")
+SEND_CHANNEL_NAME = os.getenv("SEND_CHANNEL_NAME")
 SEARCH_WORDS = os.getenv("SEARCH_WORDS")
 ADDITIONAL_TEXT_TO_MESSAGE = os.getenv("ADDITIONAL_TEXT_TO_MESSAGE")
 
-client = TelegramClient('spainbotsecure', int(API_ID), API_HASH)
+client = TelegramClient('bot', int(API_ID), API_HASH)
 
 
 # Ищет конкретные сообщения в чате и переотправляет их в другой чат.
-@client.on(events.NewMessage(chats=[int(LISTENING_CHANNEL_ID)]))
+@client.on(events.NewMessage(chats=[LISTENING_CHANNEL_NAME]))
 async def search_new_message(event):
     for word in SEARCH_WORDS.split(","):
         if word in event.raw_text:
             await client.send_message(
-                int(SEND_CHANNEL_ID),
+                SEND_CHANNEL_NAME,
                 f"{event.raw_text}\n{ADDITIONAL_TEXT_TO_MESSAGE}"
                 if len(ADDITIONAL_TEXT_TO_MESSAGE)
                 else event.raw_text
@@ -29,9 +29,9 @@ async def search_new_message(event):
 
 # Проверка работоспособности.
 # Ожидает сообщение "/check" в чате указанного для отправки сообщений.
-@client.on(events.NewMessage(chats=[int(SEND_CHANNEL_ID)], pattern='/check'))
+@client.on(events.NewMessage(chats=[SEND_CHANNEL_NAME], pattern='/check'))
 async def check(event):
-    await client.send_message(int(SEND_CHANNEL_ID), 'ACTIVE')
+    await client.send_message(SEND_CHANNEL_NAME, 'ACTIVE')
 
 
 client.start()
